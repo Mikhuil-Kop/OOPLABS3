2f554b2ac6e3a7e341ae99ac47df27b4bf9ee601
Mike Kopotov <mikekopotov@iMac-Alexey.local>
2019-09-20 20:08:43 +0300

    Лаба 2 вариант 1 сделана полностью

--- Lab2/src/com/company/CircleTest.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class CircleTest {
-    public static void main(String[] args) {
-        // write your code here
-    }
-}
--- Lab2_var1/src/com/company/Main.java
+++ Lab2_var1/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        CircleTest.main(args);
+	    CircleTest.main(args);
+	    System.out.println("Программа завершена");
     }
 }
